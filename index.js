8fe3abd60c579807a4f8d94edc3a4e7f45b6ffde
Erick <erick@nalabs.io>
2020-01-19 02:16:25 -0300

    Primer commit

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const fs = require('fs');
 const { Credentials } = require('uport-credentials');
-console.log(Credentials.createIdentity());
+const file_pk = "priv_key.txt";
+const file_credential = 'credential.txt'
+const new_credential = Credentials.createIdentity();
+
+fs.writeFile(file_pk, new_credential.privateKey, (err) =>{
+    if(err) throw err;
+    console.log(`PK almacenada '${file_pk}'`);
+})
+
+fs.writeFile(file_credential, new_credential, (err) =>{
+    if(err) throw err;
+    console.log(`Credencial almacenada '${file_credential}'`);
+})
